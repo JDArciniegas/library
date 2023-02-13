dd5c00cf962265bdae205001d677af7e4aa10572
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-02-13 08:51:39 -0700

    -set up skeleton structure

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read){
   this.title = title
   this.author = author
@@ -8,5 +10,5 @@ function Book(title, author, pages, read){
   }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolien', 295, 'not read')
-console.log(theHobbit.info());
+function addBookToLibrary(){};
+
