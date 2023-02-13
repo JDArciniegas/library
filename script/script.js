32c0cd5b83901597529dd69d446d4c5dc724efc5
Juan Arciniegas <juand.arciniegas@gmail.com>
2023-02-13 09:34:16 -0700

    saved all variables, added event to push input values into Book >> myLibrary

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
-let myLibrary = [];
+// variables
+const bookName = document.querySelector('#book-name');
+const bookWriter = document.querySelector('#book-writer');
+const bookPages = document.querySelector('#book-pages');
+const readStatus = document.querySelector('#read-status');
+const submitButton = document.querySelector('#submit');
+
+// base database book
+const gameOfThrones = new Book('Game of Thrones','George R. R. Martin', 1000, 'Not Read');
+const harryPotter = new Book('Harry Potter & the philosofer stone', 'J. K. Rowling', 700, 'Not Read');
+
+
+let myLibrary = [gameOfThrones, harryPotter];
 
 function Book(title, author, pages, read){
   this.title = title
@@ -10,5 +22,16 @@ function Book(title, author, pages, read){
   }
 }
 
-function addBookToLibrary(){};
+function addBookToLibrary(){
+  let newBook = new Book(bookName, bookWriter, bookPages, readStatus);
+  myLibrary.push(newBook);
+  console.log(myLibrary);
+};
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBookToLibrary()
+})
+
+
 
